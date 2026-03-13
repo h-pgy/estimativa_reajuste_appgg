81da09ecf403744e459e84c8ccf2e70d9cc532f9
h-pgy <hpougy@gmail.com>
2026-03-12 23:49:21 -0300

    improved refactor of initial steps

---
+++ core/simulator/initial_command.py
@@ -0,0 +1,7 @@
+from .simulation_command import SimulationCommand
+
+
+class InitialCommand(SimulationCommand):
+
+    def __init__(self) -> None:
+        super().__init__()
---
+++ core/simulator/steps/initial/__init__.py
@@ -0,0 +1,7 @@
+from .data_loader import Loader
+from .sintetic_data import RecemNomeadoData
+from .data_preparator import Preparator
+
+load_original_data = Loader()
+make_sintetic_recem_nomeados_data = RecemNomeadoData()
+prepare_original_data = Preparator()
