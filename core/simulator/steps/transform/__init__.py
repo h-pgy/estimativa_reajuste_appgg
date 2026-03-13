17eab0a21694a5b21352b49fc575618f4f544d35
h-pgy <hpougy@gmail.com>
2026-03-13 13:21:11 -0300

    calculo vale alimentacao

--- core/simulator/steps/transform/__init__.py
+++ core/simulator/steps/transform/__init__.py
@@ -1,6 +1,8 @@
 from .vencimento_atual import Vencimento
 from .ferias_decimo_terceiro import DecimoTerceiro, TercoAdicionalFerias
+from .vale_alimentacao import ValeAlimentacao
 
 obter_vencimento = Vencimento()
 calc_decimo_terceiro = DecimoTerceiro()
 calc_terco_adicional = TercoAdicionalFerias()
+calc_vale_alimentacao = ValeAlimentacao()
