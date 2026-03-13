496b92bb12de8fed831adc4214c6f1a7aa8bd3ab
h-pgy <hpougy@gmail.com>
2026-03-13 16:11:07 -0300

    total mensal

---
+++ core/simulator/steps/transform/total_mensal.py
@@ -0,0 +1,27 @@
+from core.models.servidores import ServidorVencimentoDataframe
+import pandas as pd
+
+class ValeAlimentacao:
+
+    colunas_total = [
+        'vencimento',
+        'decimo_terceiro',
+        'terco_ferias',
+        'contribuicao_iprem',
+        'contribuicao_inss',
+        'previdencia_complementar',
+        'vale_alimentacao'
+    ]
+
+    
+    def __call__(self, df:pd.DataFrame)->pd.DataFrame:
+
+        df = ServidorVencimentoDataframe.validate(df)
+
+        for col in self.colunas_total:
+            if col not in df.columns:
+                raise ValueError(f'Precisa calcular {col} antes.')
+
+        df['valor_total'] = df[self.colunas_total].sum(axis=1)
+        
+        return df
