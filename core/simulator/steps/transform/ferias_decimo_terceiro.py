bf907651979653b34e060231027c8bedc915ed1a
h-pgy <hpougy@gmail.com>
2026-03-13 12:43:05 -0300

    calculo terco adicional e decimo terceiro

---
+++ core/simulator/steps/transform/ferias_decimo_terceiro.py
@@ -0,0 +1,31 @@
+from core.models.servidores import ServidorVencimento, ServidorVencimentoDataframe
+import pandas as pd
+from pandera.typing import Series
+
+class DecimoTerceiro:
+
+    def calcular_decimo_terceiro(self, row:Series[ServidorVencimento])->float:
+
+        return round(row['vencimento']/12, 2)
+    
+    def __call__(self, df:pd.DataFrame)->pd.DataFrame:
+
+        df = ServidorVencimentoDataframe.validate(df)
+
+        df['decimo_terceiro'] = df.apply(self.calcular_decimo_terceiro, axis=1)
+
+        return df
+    
+class TercoAdicionalFerias:
+
+    def calcular_terco_adicional(self, row:Series[ServidorVencimento])->float:
+
+        return round(row['vencimento']/3, 2)
+    
+    def __call__(self, df:pd.DataFrame)->pd.DataFrame:
+
+        df = ServidorVencimentoDataframe.validate(df)
+
+        df['terco_ferias'] = df.apply(self.calcular_terco_adicional, axis=1)
+
+        return df
