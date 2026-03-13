0286090f8b3d76852d169e3349e000737a7141b0
h-pgy <hpougy@gmail.com>
2026-03-13 13:07:25 -0300

    vencimentos

--- core/simulator/steps/transform/__init__.py
+++ core/simulator/steps/transform/__init__.py
@@ -1,4 +1,6 @@
+from .vencimento_atual import Vencimento
 from .ferias_decimo_terceiro import DecimoTerceiro, TercoAdicionalFerias
 
+obter_vencimento = Vencimento()
 calc_decimo_terceiro = DecimoTerceiro()
 calc_terco_adicional = TercoAdicionalFerias()
