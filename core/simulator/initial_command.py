17420620c9652edb4231484945b7af10346a78a8
h-pgy <hpougy@gmail.com>
2026-03-14 19:09:27 -0300

    bugfixes no schema do servidor

--- core/simulator/initial_command.py
+++ core/simulator/initial_command.py
@@ -1,13 +1,13 @@
 from .simulation_command import SimulationCommand
 from .steps.initial import load_original_data, make_sintetic_recem_nomeados_data, prepare_original_data
-from core.utils.json import load_json
+from core.utils.json import load_json_to_dataframe
 from config import CARGO_BASE, TABELA_ORIGINAL, QTD_RECEM_NOMEADOS
 
 class InitialCommand(SimulationCommand):
 
     def __init__(self, fpath_tabela_original:str=TABELA_ORIGINAL, qtd_recem_nomeados:int=QTD_RECEM_NOMEADOS) -> None:
         super().__init__()
-        self.tabela_original = load_json(fpath_tabela_original)
+        self.tabela_original = load_json_to_dataframe(fpath_tabela_original)
         self.qtd_recem_nomeados = qtd_recem_nomeados
         self.load_steps()
 
--- core/utils/json.py
+++ core/utils/json.py
@@ -1,5 +1,6 @@
 import json
 from typing import Optional, Union
+import pandas as pd
 from .path import solve_fpath
 
 
@@ -10,3 +11,10 @@ def load_json(fname:str, dirname:Optional[str]=None)->Union[list, dict]:
         data = json.load(f)
     
     return data
+
+def load_json_to_dataframe(fname:str, dirname:Optional[str]=None)->pd.DataFrame:
+
+    data = load_json(fname, dirname)
+    if type(data) is not list:
+        raise ValueError('Expected a list of dictionaries to convert to DataFrame')
+    return pd.DataFrame(data)
