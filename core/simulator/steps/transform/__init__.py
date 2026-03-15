370540e1165ad0056e62547a18cad0fae624c476
h-pgy <hpougy@gmail.com>
2026-03-14 23:11:45 -0300

    bugfix total mensal - class name - and on vencimento allowing to define tabela on initialization

--- core/simulator/steps/transform/__init__.py
+++ core/simulator/steps/transform/__init__.py
@@ -2,11 +2,12 @@ from .vencimento_atual import Vencimento
 from .ferias_decimo_terceiro import DecimoTerceiro, TercoAdicionalFerias
 from .vale_alimentacao import ValeAlimentacao
 from .previdencia import ContribuicaoIprem, ContribuicaoINSS, PrevidenciaComplementar
+from .total_mensal import TotalMensal
 
-obter_vencimento = Vencimento()
 calc_decimo_terceiro = DecimoTerceiro()
 calc_terco_adicional = TercoAdicionalFerias()
 calc_vale_alimentacao = ValeAlimentacao()
 calc_contrib_iprem = ContribuicaoIprem()
 calc_contrib_inss = ContribuicaoINSS()
 calc_prev_complementar = PrevidenciaComplementar()
+total_mensal = TotalMensal
